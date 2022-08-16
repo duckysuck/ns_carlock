10ddb8393c38abeb57526635db61ecef2023ae6b
Nick's Scripts <95194821+NickScripts@users.noreply.github.com>
2022-08-15 21:31:39 -0500

    Add files via upload

---
+++ ns_carlock/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+Config.KeyBind = 'U'
+Config.LockedMessage = 'Vehicle Locked'
+Config.UnlockedMessage = 'Vehicle Unlocked'
+Config.LockingMessage = 'Locking Vehicle'
+Config.UnlockingMessage = 'Unlocking Vehicle'
